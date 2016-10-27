2f586d3d4eea019325c7bedf01338afa2bf4fa49
Eric Goodwin <eric@ericgoodwin.com>
2016-10-26 20:31:42 -0700

    Heroku deployment updates

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :oauth2_example, OAuth2Example.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "serene-sea-38795.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :oauth2_example, OAuth2Example.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  database: "oauth2_example_prod",
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,5 +69,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
-
